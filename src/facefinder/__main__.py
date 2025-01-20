27258b3bbd8715029b157e5540cdc4a6547d1671
saltchicken <john.eicher89@gmail.com>
2025-01-19 20:03:02 -0800

    Insert the name of the embedding

--- src/facefinder/__main__.py
+++ src/facefinder/__main__.py
@@ -15,7 +15,7 @@ def main():
         "--embedding", action="store_true", help="Converting folder of images to faces"
     )
     parser.add_argument(
-        "--insert", action="store_true", help="Insert the retrieved embedding"
+        "--insert", default=None, help="Insert the retrieved embedding"
     )
     parser.add_argument(
         "--check", action="store_true", help="Check the retrieved embedding"
@@ -26,12 +26,16 @@ def main():
     # TODO: Check if unnecessary flags are set: Both insert and check should not be set to true
 
     if args.embedding:
+        # TODO: Only run this if the command is valid: If nothing set for --insert then it will error but embedding will run
         embedding = get_embedding(args.input)
         if args.insert:
-            insert_embedding("test", embedding)
+            insert_embedding(args.insert, embedding)
         elif args.check:
             result = check_embedding(embedding)
             print(f"Result: {result}")
+        else:
+            # TODO: This never gets called because --insert expects a value
+            print("No action for embedding specified")
     else:
         detect_face(args.input)
 
