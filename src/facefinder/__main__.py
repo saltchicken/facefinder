87b5e2d28daf943bf7893c85b6d835a0f2025db1
saltchicken <john.eicher89@gmail.com>
2025-01-20 12:50:10 -0800

    Properly handle bad input to cli and close db connection gracefully

--- src/facefinder/__main__.py
+++ src/facefinder/__main__.py
@@ -1,15 +1,36 @@
 import argparse
+import shlex
 # from deepface import DeepFace
 from .analyze import detect_face, get_embedding
 from .postgresql_client import EmbeddingDatabase
 
-def main():
-    # embedding()
+db = EmbeddingDatabase()
+
+def interactive_prompt():
+    parser = build_parser()
+
+    while True:
+        user_input = input("FaceFinder>>> ").strip()
+        if user_input.lower() == "exit":
+            print("Closing facefinder")
+            break
+        elif user_input.lower() == "help" or user_input == "--help":
+            parser.print_help()
+        else:
+            try:
+                args = parser.parse_args(shlex.split(user_input))
+            except SystemExit as e:
+                continue
+            command_dispatcher(args)
+
+    db.close()
+
+def build_parser():
     parser = argparse.ArgumentParser(
         description="Detect and recognize face with deepface"
     )
 
-    parser.add_argument("input", help="Input image")
+    parser.add_argument("-i", "--input", help="Input image")
     # parser.add_argument(
     #     "-o", "--output", default="output.png", type=str, help="Output folder"
     # )
@@ -22,15 +43,13 @@ def main():
     parser.add_argument(
         "--check", action="store_true", help="Check the retrieved embedding"
     )
+    return parser
 
-    args = parser.parse_args()
-
+def command_dispatcher(args):
     # TODO: Check if unnecessary flags are set: Both insert and check should not be set to true
-
     if args.embedding:
         # TODO: Only run this if the command is valid: If nothing set for --insert then it will error but embedding will run
         embedding = get_embedding(args.input)
-        db = EmbeddingDatabase()
         if args.insert:
             db.insert_embedding(args.insert, embedding)
         elif args.check:
@@ -40,6 +59,15 @@ def main():
             # TODO: This never gets called because --insert expects a value
             print("No action for embedding specified")
     else:
-        print("hello")
         detect_face(args.input)
+
+
+def main():
+    parser = build_parser()
+
+    args = parser.parse_args()
+    command_dispatcher(args)
+    db.close()
+
+
 
--- src/facefinder/postgresql_client.py
+++ src/facefinder/postgresql_client.py
@@ -6,19 +6,23 @@ load_dotenv()
 
 class EmbeddingDatabase:
     def __init__(self):
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
         # Establish a persistent connection when the object is initialized
         self.connection_string = f"postgres://{os.getenv('USER')}@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DB_NAME')}"
         print(self.connection_string)
         self.conn = psycopg.connect(self.connection_string)
+
+    def close(self):
+        print("Closing connection to database")
+        if self.conn:
+            self.conn.close()
 
     def __enter__(self):
         # Allows usage in a context manager
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        print("Closing connection to database")
-        if self.conn:
-            self.conn.close()
+        self.close()
 
     def insert_embedding(self, name, embedding):
         with self.conn.cursor() as cursor:
