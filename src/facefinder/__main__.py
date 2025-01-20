bcf4d062a50c3500bc2d2acd720104c69b6c2821
saltchicken <john.eicher89@gmail.com>
2025-01-20 12:57:19 -0800

    Remove unnecessary argument

--- src/facefinder/__main__.py
+++ src/facefinder/__main__.py
@@ -1,6 +1,5 @@
 import argparse
 import shlex
-# from deepface import DeepFace
 from .analyze import detect_face, get_embedding
 from .postgresql_client import EmbeddingDatabase
 
@@ -35,9 +34,6 @@ def build_parser():
     #     "-o", "--output", default="output.png", type=str, help="Output folder"
     # )
     parser.add_argument(
-        "--embedding", action="store_true", help="Converting folder of images to faces"
-    )
-    parser.add_argument(
         "--insert", default=None, help="Insert the retrieved embedding"
     )
     parser.add_argument(
@@ -46,19 +42,15 @@ def build_parser():
     return parser
 
 def command_dispatcher(args):
-    # TODO: Check if unnecessary flags are set: Both insert and check should not be set to true
-    if args.embedding:
-        # TODO: Only run this if the command is valid: If nothing set for --insert then it will error but embedding will run
+    if args.insert:
         embedding = get_embedding(args.input)
-        if args.insert:
-            db.insert_embedding(args.insert, embedding)
-        elif args.check:
-            result = db.check_embedding(embedding)
-            print(f"Result: {result}")
-        else:
-            # TODO: This never gets called because --insert expects a value
-            print("No action for embedding specified")
+        db.insert_embedding(args.insert, embedding)
+    elif args.check:
+        embedding = get_embedding(args.input)
+        result = db.check_embedding(embedding)
+        print(f"Result: {result}")
     else:
+        print("Running default detect_face")
         detect_face(args.input)
 
 
