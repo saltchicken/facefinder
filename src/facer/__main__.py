4b02136ba859cbcad9c9a75da44f0a3d723f09ef
saltchicken <john.eicher89@gmail.com>
2025-01-25 13:00:14 -0800

    facer functionality now its own class

--- src/facer/__main__.py
+++ src/facer/__main__.py
@@ -1,19 +1,16 @@
 import argparse
 import shlex
-from fripper.ffmpeg_cmd import grab_thumbnails
-import tempfile
-from .analyze import detect_face, get_embedding, get_embeddings_from_folder, match_list_of_embeddings
-from .postgresql_client import EmbeddingDatabase
+from .facer import Facer
 
-db = EmbeddingDatabase()
 
 def interactive_prompt():
     parser = build_parser()
+    facer = Facer()
 
     while True:
-        user_input = input("FaceFinder>>> ").strip()
+        user_input = input("Facer >>> ").strip()
         if user_input.lower() == "exit":
-            print("Closing facefinder")
+            print("Closing facer")
             break
         elif user_input.lower() == "help" or user_input == "--help":
             parser.print_help()
@@ -22,9 +19,8 @@ def interactive_prompt():
                 args = parser.parse_args(shlex.split(user_input))
             except SystemExit as e:
                 continue
-            command_dispatcher(args)
-
-    db.close()
+            command_dispatcher(args, facer)
+    facer.db.close()
 
 def build_parser():
     parser = argparse.ArgumentParser(description="Detect and recognize face with deepface")
@@ -33,55 +29,30 @@ def build_parser():
     insert_parser = subparsers.add_parser("insert", help="Insert the retrieved embedding")
     insert_parser.add_argument("input", help="Input image path")
     insert_parser.add_argument("name", help="Name of person in image")
-    insert_parser.add_argument("--folder", action="store_true", help="If specified, will prompt for multiple faces to insert into database from input folder")
 
     check_parser = subparsers.add_parser("match", help="Match the retrieved embedding")
     check_parser.add_argument("input", help="Input image path")
-    check_parser.add_argument("--video", action="store_true", help="Match the faces in a video.")
 
     return parser
 
-def command_dispatcher(args):
+def command_dispatcher(args, facer):
     if args.command == "insert":
-        if args.folder:
-            embeddings = get_embeddings_from_folder(args.input)
-            for embedding in embeddings:
-                db.insert_embedding(args.name, embedding)
-        else:
-            try:
-                embedding = get_embedding(args.input)
-                db.insert_embedding(args.name, embedding)
-            except Exception as e:
-                print(f"Embedding failed to insert. Error: {e}")
-            # NOTE: The following code block automatically averages on insert.
-            # if db.does_name_exist(args.name):
-            #     db.average_embedding(args.name, embedding)
-            # else:
-            #     db.insert_embedding(args.name, embedding)
+        facer.insert(args.name, args.input)
     elif args.command == "match":
-        if args.video:
-            with tempfile.TemporaryDirectory() as temp_dir:
-                image_paths = grab_thumbnails(args.input, temp_dir)
-                result = match_list_of_embeddings(image_paths, db)
-                print(result)
-        else:
-            try:
-                embedding = get_embedding(args.input)
-                result = db.check_embedding(embedding)
-                print(f"Result: {result}")
-            except Exception as e:
-                print(f"Embedding failed to get a match. Error: {e}")
+        facer.match(args.input)
     else:
-        print("Running default detect_face")
-        detect_face(args.input)
+        print("This shouldn't happen")
+        # print("Running default detect_face")
+        # detect_face(args.input)
 
 
 def main():
     parser = build_parser()
 
     args = parser.parse_args()
-    command_dispatcher(args)
-    db.close()
+    facer = Facer()
+    command_dispatcher(args, facer)
+    facer.db.close()
 
 
 
---
+++ src/facer/facer.py
@@ -0,0 +1,70 @@
+from fripper.ffmpeg_cmd import grab_thumbnails
+import tempfile
+from pathlib import Path
+import mimetypes
+from .analyze import get_embedding, get_embeddings_from_folder, match_list_of_embeddings
+from .postgresql_client import EmbeddingDatabase
+
+def classify_path(path):
+    path = Path(path)
+
+    if not path.exists():
+        return None
+
+    if path.is_dir():
+        return "folder"
+
+    mime_type, _ = mimetypes.guess_type(path)
+    if mime_type:
+        if mime_type.startswith("image/"):
+            return "image"
+        elif mime_type.startswith("video/"):
+            return "video"
+        else:
+            return None
+    return None
+
+class Facer:
+    def __init__(self):
+        self.db = EmbeddingDatabase()
+
+    def insert(self, name, input_file):
+        match classify_path(input_file):
+            case "folder":
+                embeddings = get_embeddings_from_folder(input_file)
+                for embedding in embeddings:
+                    self.db.insert_embedding(name, embedding)
+            case "image":
+                try:
+                    embedding = get_embedding(input_file)
+                    self.db.insert_embedding(name, embedding)
+                except Exception as e:
+                    print(f"Failed to insert image: {e}")
+            case "video":
+                print("Not implemented for video")
+                # with tempfile.TemporaryDirectory() as temp_dir:
+                #     temp_dir_path = Path(temp_dir)
+                #     output_file = temp_dir_path / "image.jpg"
+                #     grab_thumbnails(input_file, output_file)
+            case _:
+                print("Invalid input file")
+
+    def match(self, input_file, threshold=0.85):
+        match classify_path(input_file):
+            case "folder":
+                print("Not implemented for folders")
+            case "image":
+                try:
+                    embedding = get_embedding(input_file)
+                    result = self.db.check_embedding(embedding)
+                    print(f"Result: {result}")
+                except Exception as e:
+                    print(f"Embedding failed to get match. Error: {e}")
+            case "video":
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    image_paths = grab_thumbnails(input_file, temp_dir)
+                    result = match_list_of_embeddings(image_paths, self.db)
+                    print(result)
+            case _:
+                print("Invalid input file")
+
